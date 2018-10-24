df97f3f29c3cb06e139fcbeba180595a7b0c3f89
contrepoint <contrepoint21@gmail.com>
2018-10-24 22:19:13 +0800

    set up dictionary data structure and default dictionary file

--- lib/pronouncing.rb
+++ lib/pronouncing.rb
@@ -1,4 +1,16 @@
 require "pronouncing/version"
 
 module Pronouncing
+  class Dictionary
+    def initialize(dictionary = nil)
+      dictionary ||= File.join(File.dirname(__FILE__), "/dictionary/cmudict-0.7b.txt")
+
+      @rhymes = Hash.new{ |hash, key| hash[key] = [] }
+      @words = Hash.new do |hash, key|
+        hash[key] = {
+          phones: [], stresses: [], syllables: [], rhyming_parts: []
+        }
+      end
+    end
+  end
 end
